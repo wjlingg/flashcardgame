83b39c3d8b40f17ccdb415663cc42b7b35a1c1c9
wjlingg <e0309488@u.nus.edu>
2022-05-11 22:16:11 +0800

    Added countdown timer

--- FlashCardGame/MainWindow.xaml.cs
+++ FlashCardGame/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace FlashCardGame
 {
@@ -20,9 +21,40 @@ namespace FlashCardGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
+        TimeSpan _time;
+
         public MainWindow()
         {
             InitializeComponent();
+            timer.Interval = new TimeSpan(0, 0, 1); // timer will "tick" every 1 second
+            timer.Tick += Timer_Count; // Process time every 1 second
+            _time = TimeSpan.FromSeconds(10);
+        }
+
+        private void EnterButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Timer_Count(object sender, object e)
+        {
+            _time = _time.Add(TimeSpan.FromSeconds(-1));
+            timerText.Text = _time.ToString("c");
+
+            if (timerText.Text == "00:00:00")
+            {
+                timer.Stop();
+            }
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Reset all information to initial state
+
+            timerText.Text = _time.ToString("c");
+            startButton.IsEnabled = false;
+            timer.Start();
         }
     }
 }
